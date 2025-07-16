1ff7040dea8c5322228d4ccd1454814d9d224662
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-07-16 18:43:09 -0400

    Add more Icons for the SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import ascending from '../assets/graph-up-arrow.svg'
+import descending from '../assets/graph-down-arrow.svg'
+import most from '../assets/chevron-double-up.svg'
+import best from '../assets/star-fill.svg'
 
 function SideBar({ratingValue, ratingCount, priceLtoH, priceHtoL}) {
     const [active, setActive] = useState(null);
@@ -10,22 +14,31 @@ function SideBar({ratingValue, ratingCount, priceLtoH, priceHtoL}) {
                 ratingValue();
                 setActive("highest");
             }} className = {active === "highest" ? "option active" : "option"}>
-                Highest Ratings</div>
+                <img src={best}></img>
+                <span>Highest Ratings</span>
+                
+            </div>
             <div onClick={() => {
                 ratingCount();
                 setActive("most");
             }} className = {active === "most" ? "option active" : "option"}>
-                Most Ratings</div>
+                <img src={most}></img>
+                <span>Most Ratings</span>
+            </div>
             <div onClick={() => {
                 priceLtoH();
                 setActive("hTol");
             }} className = {active === "hTol" ? "option active" : "option"}>
-                Price High to Low</div>
+                <img src={descending}></img>
+                <span>Price High to Low</span>
+            </div>
             <div onClick={() => {
                 priceHtoL();
                 setActive("lToh");
             }} className = {active === "lToh" ? "option active" : "option"}>
-                Price Low to High</div>
+                <img src={ascending}></img>
+                <span>Price Low to High</span>
+            </div>
         </div>
     );
 }
