8e000f6585850382022927d6eb2c67a42098056b
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-07-17 14:19:59 -0400

    Add Vercel config for Deployment

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,19 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import App from './App.jsx'
 
-describe('App Component', () => {
-    it("renders correct heading", () => {
-        render(<App></App>);
-        expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
-    });
+describe('App component', () => {
+  it('shows the NavBar logo text "Steam Steals!"', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    // Look for the exact text in the NavBar
+    const logoText = screen.getByText(/steam steals!/i);
+    expect(logoText).toBeInTheDocument();
+  });
 });
