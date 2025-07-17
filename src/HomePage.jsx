987b6affc390faf1059e08d8bd38d0f66c22666f
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-07-17 18:23:50 -0400

    Add Text to Home Screen

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,10 @@
 function HomePage() {
     return (
         <div className="homePage">
+            <h1>Welcome to Steam Steals!</h1>
+            <p>Browse our site for a selection of Steam Games, each with the lowest price 
+                you could get them for!
+            </p>
         </div>
     );
 }
